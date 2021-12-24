ee86f8f64cb495c764a44ff2adb14a5fd94d1425
Brady Vossler <bradyvossler@gmail.com>
2021-12-24 10:15:23 -0600

    create Create page

--- src/main.js
+++ src/main.js
@@ -2,20 +2,19 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-// 1. Define route components.
-// These can be imported from other files
+//? IMPORT PAGE COMPONENTS
 import Home from './pages/Home.vue';
+import Create from './pages/CreateRecipea.vue';
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
-const routes = [{ path: '/', component: Home }];
+//? DEFINE ROUTES, EACH PAGE COMPONENT GETS ONE
+const routes = [
+  { path: '/', component: Home },
+  { path: '/create', component: Create },
+];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
+//? SET UP THE ROUTER
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  // Using recommended default history setup
   history: createWebHistory(),
   routes, // short for `routes: routes`
 });
--- src/pages/CreateRecipea.vue
+++ src/pages/CreateRecipea.vue
@@ -1,7 +1,15 @@
 <template>
-  <div></div>
+  <h1>CREATE RECIPEA</h1>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Create',
+  components: {},
+  data() {
+    return {};
+  },
+};
 </script>
+
+<style></style>
